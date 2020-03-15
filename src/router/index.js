1f452f976c3d24db0951b9859dcc7e6d1d65980c
liu lei <1206821643@qq.com>
2020-03-15 21:23:10 +0800

    配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,58 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home'
 
 Vue.use(VueRouter)
+// 按需加载方式配置路由
+const Layout = () => import('@/views/layout')
+// 按需引入下面的4个二级路由
+const home = () => import('@/views/home')
+const question = () => import('@/views/question')
+const video = () => import('@/views/video')
+const user = () => import('@/views/user')
+// 其他的一级路由
+const chat = () => import('@/views/user/chat')// 小智同学
+const login = () => import('@/views/login')// 登录模式
+const profile = () => import('@/views/user/profile')// 编辑资料
+const search = () => import('@/views/search')// 搜索中心
+const searchresult = () => import('@/views/search/result')// 搜索结果
+const article = () => import('@/views/article')// 文章中心
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Layout, // 布局组件，一级路由
+    children: [{
+      path: '', // 如果path什么都不写 默认就是二级路由的默认组件
+      component: home
+    }, {
+      path: '/question', // 问答组件
+      component: question
+    }, {
+      path: '/video',
+      component: video
+    }, {
+      path: '/user',
+      component: user//
+    }, {
+      path: '/user/chat',
+      component: chat// 一级路由，小智同学
+    }, {
+      path: '/login',
+      component: login// 一级路由，登录
+    }, {
+      path: '/user/profile',
+      component: profile// 一级路由，编辑资料
+    }, {
+      path: '/search',
+      component: search// 一级路由，搜索中心
+    }, {
+      path: '/search/result',
+      component: searchresult// 一级路由，搜索中心
+    }, {
+      path: '/article',
+      component: article// 一级路由，文章详情
+    }]
   }
 
 ]
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>公共布局</div>
+  <div class="container">
+
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.container{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  // 控制二级路由容器的高宽
+  .my-wrapper{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding-top: 46px;
+    padding-bottom: 50px;
+    box-sizing: border-box;
+    &.noTop{
+      padding-top: 0;
+    }
+  }
+}
 </style>
