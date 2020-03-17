589cd3a3e829925dd4b75892fae65e7207648ca7
liu lei <1206821643@qq.com>
2020-03-17 22:37:54 +0800

    登录基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,19 @@
   <div class="container">
     <!-- 搜索组件以及路由，返回上一个页面 -->
     <van-nav-bar left-arrow title="登录" @click-left="$router.back()" ></van-nav-bar>
-  </div>
+    <!-- 登录布局 -->
+    <!-- 外层需要用cell-group组件来包裹提供 边框 -->
+    <van-cell-group>
+      <van-field label="手机号" placeholder="请输入手机号" ></van-field>
+      <van-field label="验证码" placeholder="请输入验证码" >
+        <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+        <!-- 登录按钮 -->
+        <div class="login-box">
+          <van-button   round block type="info" size="small" >登录</van-button>
+        </div>
+         </div>
 </template>
 
 <script>
@@ -12,5 +24,7 @@ export default {
 </script>
 
 <style>
-
+.login-box{
+  padding: 20px;
+}
 </style>
