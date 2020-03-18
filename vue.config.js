d9b335b603af451fd815ac3061aa7086f6c18cf7
liu lei <1206821643@qq.com>
2020-03-18 13:13:04 +0800

    修改vant主题颜色

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// 覆盖蓝色主题
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          blue: '#3296fa'
+        //   blue: '#ccc'
+        }
+      }
+    }
+  }
+}
