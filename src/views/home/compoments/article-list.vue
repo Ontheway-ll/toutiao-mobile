c507375900c347821b79c7f68efd51d32d720d4e
liu lei <1206821643@qq.com>
2020-03-19 17:46:21 +0800

    article-列表文章布局

--- src/views/home/compoments/article-list.vue
+++ src/views/home/compoments/article-list.vue
@@ -3,22 +3,46 @@
   <!-- van-list 可以帮助我们实现上拉加载  需要一些变量 -->
   <!-- 这里放置这个div的目的是 形成滚动条, 因为我们后期要做 阅读记忆 -->
   <!-- 阅读记忆  上次你阅读到哪  回来之后还是哪-->
-<div class="scroll-wrapper">
+  <div class="scroll-wrapper">
     <!-- 文章列表 -->
-      <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
+    <!-- van-list组件 如果不加干涉, 初始化完毕 就会检测 自己距离底部的长度,如果超过了限定 ,就会执行 load事件  自动把
        绑定的 loading 变成true
-      -->
-      <!-- 下拉刷新组件，包裹列表组件 -->
-      <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
-        <van-list finished-text="没有了" @load="onLoad"  v-model="uploading" :finished="finished">
+    -->
+    <!-- 下拉刷新组件，包裹列表组件 -->
+    <van-pull-refresh v-model="downLoading" @refresh="onRefresh" :success-text="successText">
+      <van-list finished-text="没有了" @load="onLoad" v-model="uploading" :finished="finished">
         <!-- 循环的内容 -->
         <van-cell-group>
-          <van-cell v-for="item in articles" :key="item" title="每股熔断" :value="`天台排队`+item"></van-cell>
+          <van-cell v-for="item in articles" :key="item" >
+            <!-- 放置元素 文章列表的循环项  无图  单图  三图 -->
+            <div class="article_item">
+              <!-- 标题 -->
+              <h3 class="van-ellipsis">2020不平凡的一年</h3>
+              <!-- 三图 -->
+              <div class="img_box">
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+                <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div>
+              <!-- 放置一张图 ，暂时隐藏-->
+              <!-- <div class="img_box">
+                <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              </div> -->
+              <!-- 底部信息 -->
+              <div class="info_box">
+                <span>你像一阵风</span>
+                <span>8评论</span>
+                <span>10分钟前</span>
+                <span class="close">
+                  <van-icon name="cross"></van-icon>
+                </span>
+              </div>
+            </div>
+          </van-cell>
         </van-cell-group>
       </van-list>
-      </van-pull-refresh>
-
-</div>
+    </van-pull-refresh>
+  </div>
 </template>
 
 <script>
@@ -29,12 +53,12 @@ export default {
       uploading: false, // 表示是否开启了上拉加载 默认值false
       finished: false, // 表示 是否已经完成所有数据的加载
       downLoading: false, // 下载刷新状态 表示是否正在下拉刷新,变量名自己写
-      successText: ''// 刷新成功的文本
+      successText: '' // 刷新成功的文本
     }
   },
   methods: {
     onLoad () {
-    //   console.log('开始加载数据')
+      //   console.log('开始加载数据')
       // 下面这么写 依然不能关掉加载状态 为什么 ? 因为关掉之后  检测机制  高度还是不够 还是会开启
       // 如果你有数据 你应该 把数据到加到list中
       // 如果想关掉
@@ -47,7 +71,10 @@ export default {
         this.finished = true
       } else {
         // Array.from()方法就是将一个类数组对象或者可遍历对象转换成一个真正的数组。
-        const arr = Array.from(Array(15), (value, index) => this.articles.length + index + 1)
+        const arr = Array.from(
+          Array(15),
+          (value, index) => this.articles.length + index + 1
+        )
         // 上拉加载 不是覆盖之前的数据  应该把数据追加到数组的队尾
         this.articles.push(...arr)
         // 添加完数据 需要手动的关掉 loading
@@ -56,7 +83,10 @@ export default {
     },
     onRefresh () {
       setTimeout(() => {
-        const arr = Array.from(Array(2), (value, index) => '追加' + (index + 1))
+        const arr = Array.from(
+          Array(2),
+          (value, index) => '追加' + (index + 1)
+        )
         // 下拉刷新 表示要读取最新的数据 而且最新的数据要添加到数据头部
         this.articles.unshift(...arr)
         // 手动关闭正在加载的状态
@@ -64,11 +94,49 @@ export default {
         this.successText = `更新了${arr.length}条数据`
       }, 1000)
     }
-
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.article_item{
+  h3{
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box{
+    display: flex;
+    justify-content: space-between;
+    .w33{
+      width: 33%;
+      height: 90px;
+    }
+    .w100{
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box{
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span{
+      padding-right: 10px;
+      &.close{
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
