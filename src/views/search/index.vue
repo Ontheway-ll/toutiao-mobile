dd07851e7fa31cedfbaa35822438cfaa1b998d4c
liu lei <1206821643@qq.com>
2020-03-23 23:17:58 +0800

    搜索关键词 和历史记录及联想搜索

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,13 +3,17 @@
     <!-- 搜索组件一级路由   返回上一个页面-->
     <van-nav-bar left-arrow title="搜索中心" @click-left="$router.back()"></van-nav-bar>
     <!-- 导航 -->
-    <van-search  placeholder="请输入搜索关键词" shape="round" />
-    <van-cell-group class="suggest-box" >
+    <!-- 搜索框和数据绑定，trim去除空格 -->
+    <van-search v-model.trim="q"  placeholder="请输入搜索关键词" shape="round" />
+    <!-- 联想内容  有输入内容时 显示联想 -->
+    <van-cell-group class="suggest-box" v-if="q">
       <van-cell icon="search">
         <span>java</span>
       </van-cell>
     </van-cell-group>
-    <div class="history-box">
+      <!-- 如果没有历史记录 隐藏掉  -->
+    <!-- <div class="history-box" v-if="!q">这样也可以 -->
+    <div class="history-box" v-else>
       <div class="head">
         <span>历史记录</span>
         <van-icon name="delete"></van-icon>
@@ -26,7 +30,13 @@
 
 <script>
 export default {
-  name: 'search'
+  name: 'search',
+  // 首先应该给搜索框绑定一个数据,来获取搜索的内容
+  data () {
+    return {
+      q: ''
+    }
+  }
 }
 </script>
 
